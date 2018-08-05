969bfa51db7b2f88c2811ad1024987804585ef1a
Amee Covarrubias <ameecova@yahoo.com>
2018-08-05 02:59:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,8 +10,8 @@ class Artist
     @songs = []
   end
   
-  def add_song(song)
-    song = Song.new(song)
+  def add_song(name, song)
+    song = Song.new(name, song)
     @songs << song
     song.artist = self
   end 
